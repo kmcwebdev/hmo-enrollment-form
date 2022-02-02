1af2370453010c21ce650f8ab55605dc331d0e54
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2022-02-02 13:47:38 +0800

    Update DependentForm.tsx

--- components/DependentForm.tsx
+++ components/DependentForm.tsx
@@ -282,7 +282,7 @@ const DependentForm: React.FC<DependentFormProps> = ({ setIsOpen }) => {
         type='submit'
         className={classNames(
           isLoading || !isSuccess
-            ? 'bg-gray-500 hover:bg-gray-500 focus:ring-gray-500'
+            ? 'bg-gray-400'
             : 'bg-skin-kmc-orange hover:bg-skin-kmc-orange focus:ring-skin-kmc-orange',
           'inline-flex items-center justify-center w-full px-3 py-2 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm gap-x-2 selection:focus:outline-none focus:ring-2 focus:ring-offset-2'
         )}
