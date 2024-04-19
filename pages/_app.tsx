5f4b6524a3d10aaaf24ff8e3fb817bdacefafca8
casulit <chrisgelosulit@gmail.com>
2024-04-19 12:20:00 +0800

    refactor(_app.tsx): Enhance component rendering logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,35 @@
-import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
-import '../styles/globals.css';
-import '../styles/xdatepicker.css';
+import type { AppProps } from "next/app";
+import React, { useState, ReactElement, ReactNode } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import "../styles/globals.css";
+import "../styles/xdatepicker.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyPageProps extends AppProps {
+  pageProps: {
+    dehydratedState?: any;
+    [key: string]: any;
+  };
+}
+
+function MyApp({ Component, pageProps }: MyPageProps) {
   const [queryClient] = useState(() => new QueryClient());
+
+  const renderComponent = (
+    Component: ReactNode | ReactElement | null,
+    pageProps: any
+  ) => {
+    if (typeof Component === "function") {
+      return <Component {...pageProps} />;
+    } else if (React.isValidElement(Component)) {
+      return Component;
+    }
+    return null;
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        {renderComponent(Component, pageProps)}
       </Hydrate>
     </QueryClientProvider>
   );
